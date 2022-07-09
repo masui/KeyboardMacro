3b2591df7e67b21cbdff3c5e6b613e4b17390ea4
Toshiyuki Masui <masui@pitecan.com>
2022-07-09 15:08:14 +0900

    ...

--- recent4.el
+++ recent4.el
@@ -1,9 +1,12 @@
 (setq *last-macro* "")
+(setq *keys-tail* "")
+
 
 (defun clear-kbd-macro ()
   (setq *last-macro* "")
-  (setq r (concat (recent-keys)))
-  (setq aaa (substring r (- (min 10 (length r)))))
+  (let ((r (concat (recent-keys))))
+    (setq *keys-tail*(substring r (- (min 10 (length r)))))
+    )
   )
 
 (run-with-idle-timer 2 t 'clear-kbd-macro)
@@ -20,7 +23,7 @@
 	(setq res nil)
 	(while (and (not res) (<= i (length recent)))
 	  (setq s (substring recent (- i)))
-	  (setq res (string-match (concat aaa "\\([^\C-l]+\\)\C-l$") s))
+	  (setq res (string-match (concat *keys-tail* "\\([^\C-l]+\\)\C-l$") s))
 	  (if res
 	      (setq *last-macro* (substring s (match-beginning 1) (match-end 1))))
 	  (setq i (1+ i))
