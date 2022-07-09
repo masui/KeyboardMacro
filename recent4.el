4e7d3a42d08747d9346c6c39b1a1f5753dbd3fba
Toshiyuki Masui <masui@pitecan.com>
2022-07-09 10:04:09 +0900

    ...

--- recent4.el
+++ recent4.el
@@ -2,12 +2,11 @@
 
 (defun clear-kbd-macro ()
   (setq *last-macro* "")
-  ;;(execute-kbd-macro "\C-f\C-b\C-f\C-b")
-  (setq aaa (substring (concat (recent-keys)) -20))
+  (setq r (concat (recent-keys)))
+  (setq aaa (substring r (- (min 10 (length r)))))
   )
 
 (run-with-idle-timer 2 t 'clear-kbd-macro)
-
 
 (defun exec-macro ()
   (interactive)
@@ -15,10 +14,20 @@
   (if (string= *last-macro* "")
       (progn
 	;;(setq res (string-match (concat aaa "\C-l\\([^\C-l]*\\)\C-l$") recent))
-	(setq res (string-match (concat aaa "\C-@\\([^\C-l]*\\)\C-l$") recent))
+	;;(setq res (string-match (concat aaa "\C-@\\([^\C-l]*\\)\C-l$") recent))
+
+	(setq i 1)
+	(setq res nil)
+	(while (and (not res) (<= i (length recent)))
+	  (setq s (substring recent (- i)))
+	  (setq res (string-match (concat aaa "\\([^\C-l]+\\)\C-l$") s))
+	  (if res
+	      (setq *last-macro* (substring s (match-beginning 1) (match-end 1))))
+	  (setq i (1+ i))
+	  )
 	(if res
 	    (progn
-	      (setq *last-macro* (substring recent (match-beginning 1) (match-end 1)))
+	      ;;(setq *last-macro* (substring recent (match-beginning 1) (match-end 1)))
 	      ;;(insert *last-macro*)
 	      (execute-kbd-macro *last-macro*)
 	      )
@@ -31,3 +40,10 @@
       )
     )
   )
+
+(global-set-key "\C-l" 'exec-macro)
+
+
+
+
+
